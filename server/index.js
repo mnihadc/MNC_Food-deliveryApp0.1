71cb2a031d7254c8a097ce203deb4c823841ddd6
Muhammed Nihad C <mnihadc1777@gmail.com>
2023-11-02 12:53:50 +0530

    created signUp route and controller function
    Signed-off-by: Muhammed Nihad C <mnihadc1777@gmail.com>

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 mongoose.connect(process.env.MONGO)
@@ -15,6 +16,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000!")
