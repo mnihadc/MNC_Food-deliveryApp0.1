cc0b8b7cc945a3e7124435eb1ca31690e0159be5
Muhammed Nihad C <mnihadc1777@gmail.com>
2023-11-02 12:21:35 +0530

    create mongodb cloud and connected
    Signed-off-by: Muhammed Nihad C <mnihadc1777@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,4 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log('Connected to MongoDB!');
+    }).catch((err) => {
+        console.log("error  in connection to mongodb", err);
+    })
 
 const app = express();
 
