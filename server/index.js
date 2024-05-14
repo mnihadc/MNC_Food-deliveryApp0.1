151b20fdf91963c38edaf0769fe3c7942c45336f
Muhammed Nihad C <mnihadc1777@gmail.com>
2024-05-14 20:15:54 +0530

    index.js updated
    Signed-off-by: Muhammed Nihad C <mnihadc1777@gmail.com>

--- server/index.js
+++ server/index.js
@@ -5,53 +5,51 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listingData.route.js";
 import cookieParser from 'cookie-parser';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-global.food_items = [];
-global.food_category = [];
+import path from 'path'; // Import the path module
 
 dotenv.config();
 
-// Get the directory name using import.meta.url
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+// Serve the static files from the client directory
+const clientDistPath = path.resolve(__dirname, '../client');
+app.use(express.static(clientDistPath));
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO, {
-}).then(async () => {
-    console.log('Connected to MongoDB!');
+mongoose.connect(process.env.MONGO)
+    .then(async () => { // Mark the function as async here
+        console.log('Connected to MongoDB!');
 
-    // Access the collections after the connection is established
-    const db = mongoose.connection.db;
-    const foodItemsCollection = db.collection('food-items');
-    const foodCategoryCollection = db.collection('food-category');
+        // Access the collections after the connection is established
+        const db = mongoose.connection.db;
+        const foodItemsCollection = db.collection('food-items');
+        const foodCategoryCollection = db.collection('food-category');
 
-    // Use Promise.all to wait for both queries to resolve
-    const [foodItemsData, foodCategoryData] = await Promise.all([
-        foodItemsCollection.find({}).toArray(),
-        foodCategoryCollection.find({}).toArray(),
-    ]);
+        // Use Promise.all to wait for both queries to resolve
+        const [foodItemsData, foodCategoryData] = await Promise.all([
+            foodItemsCollection.find({}).toArray(),
+            foodCategoryCollection.find({}).toArray(),
+        ]);
 
-    global.food_items = foodItemsData;
-    global.food_category = foodCategoryData;
+        global.food_items = foodItemsData;
+        global.food_category = foodCategoryData;
 
-    // Start your Express app after database setup
-    const app = express();
-    app.use(express.json());
-    app.use(cookieParser());
-    app.use('/api/user', userRouter);
-    app.use('/api/auth', authRouter);
-    app.use('/api/listing', listingRouter);
+        // Set up your routes
+        app.use('/api/user', userRouter);
+        app.use('/api/auth', authRouter);
+        app.use('/api/listing', listingRouter);
 
-    app.use(express.static(path.join(__dirname, '/client/dist')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+        // Ensure that all other routes serve the index.html file
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(clientDistPath, 'index.html'));
+        });
+
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000!');
+        });
     })
-    app.listen(3000, () => {
-        console.log("Server is running on port 3000!");
+    .catch((err) => {
+        console.log("Error in connection to MongoDB", err);
     });
-
-}).catch((err) => {
-    console.log("Error in connection to MongoDB", err);
-});
